6ee9d3a9c03862ec3ada962d03f7c9881557185f
vladprado <76523900+vladprado@users.noreply.github.com>
2023-05-09 12:39:30 -0300

    Delete ch01ex4.py

--- mywork/ch01ex4.py
+++
@@ -1,7 +0,0 @@
-sentence = input("Entre com uma sentença; ")
-
-print(f"O primeiro caracter é {sentence[0]} \
-e tem {sentence.count(sentence[0])} ocorrências.")
-         
-print(f"O último caracter é {sentence[len(sentence) - 1]} \
-e tem {sentence.count(sentence[len(sentence) - 1])} ocorrências")
