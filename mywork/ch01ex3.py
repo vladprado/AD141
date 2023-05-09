fc166ad5e8d7ece316b616687821379a525a6beb
vladprado <76523900+vladprado@users.noreply.github.com>
2023-05-09 12:39:03 -0300

    Delete ch01ex3.py

--- mywork/ch01ex3.py
+++
@@ -1,9 +0,0 @@
-text = input("Digite um texto: ")
-
-print(f"Termina com ponto: {text.endswith('.')}")
-print(f"Todos os caracteres são alfa: {text.isalpha()}")
-print(f"Tem x no texto: {text.find('x') > 0}")
-
-texto_modificado = text.replace('e', 'E')
-
-print(texto_modificado)
